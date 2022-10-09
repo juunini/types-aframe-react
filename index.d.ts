b4ed567696d055e2a6d2e29456713cd5facbe916
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-10-09 15:03:58 +0900

    Add `a-assets`, `a-asset-item` and `a-mixin`

--- index.d.ts
+++ index.d.ts
@@ -1862,5 +1862,73 @@ declare module JSX {
        */
       'windows-motion-controls'?: string;
     } | React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+
+    /**
+     * @see https://aframe.io/docs/1.3.0/core/asset-management-system.html
+     * 
+     * @property { string } timeout - Assets timed out.
+     */
+    'a-assets'?: {
+      /** Assets timed out. */
+      timeout?: string;
+    } | React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+
+    /**
+     * @see https://aframe.io/docs/1.3.0/core/asset-management-system.html
+     * 
+     * @property { string } id - Asset ID.
+     * @property { string } src - Asset source.
+     * 
+     * @example
+     * ```html
+     * <a-scene>
+     *   <!-- Asset management system. -->
+     *   <a-assets>
+     *     <a-asset-item id="horse-obj" src="horse.obj"></a-asset-item>
+     *     <a-asset-item id="horse-mtl" src="horse.mtl"></a-asset-item>
+     *     <a-mixin id="giant" scale="5 5 5"></a-mixin>
+     *     <audio id="neigh" src="neigh.mp3"></audio>
+     *     <img id="advertisement" src="ad.png">
+     *     <video id="kentucky-derby" src="derby.mp4"></video>
+     *   </a-assets>
+     * 
+     *   <!-- Scene. -->
+     *   <a-plane src="#advertisement"></a-plane>
+     *   <a-sound src="#neigh"></a-sound>
+     *   <a-entity geometry="primitive: plane" material="src: #kentucky-derby"></a-entity>
+     *   <a-entity mixin="giant" obj-model="obj: #horse-obj; mtl: #horse-mtl"></a-entity>
+     * </a-scene>
+     * ```
+     */
+    'a-asset-item'?: {
+      src?: string;
+    } | React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+
+    /**
+     * @see https://aframe.io/docs/1.3.0/core/mixins.html
+     * 
+     * Mixins provide a way to compose and reuse commonly-used sets of component properties. They are defined using the <a-mixin> element and are placed in <a-assets>. Mixins should be set with an id, and when an entity sets that id as its mixin attribute, the entity will absorb all of the mixin’s attributes.
+     * 
+     * ```html
+     * <a-scene>
+     *   <a-assets>
+     *     <a-mixin id="red" material="color: red"></a-mixin>
+     *     <a-mixin id="blue" material="color: blue"></a-mixin>
+     *     <a-mixin id="cube" geometry="primitive: box"></a-mixin>
+     *   </a-assets>
+     * 
+     *   <a-entity mixin="red cube"></a-entity>
+     *   <a-entity mixin="blue cube"></a-entity>
+     * </a-scene>
+     * ```
+     * 
+     * The entity with red cube will take the properties from the red mixin and the cube mixin in that order. Likewise with the blue cube. Conceptually, the entities above expand to:
+     * 
+     * ```html
+     * <a-entity material="color: red" geometry="primitive: box"></a-entity>
+     * <a-entity material="color: blue" geometry="primitive: box"></a-entity>
+     * ```
+     */
+    'a-mixin'?: JSX.IntrinsicElements['a-entity'];
   }
 }
